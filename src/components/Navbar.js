eabc105a76005136e15978ab802e2f3ea90f72c8
JJ <jumakrk@gmail.com>
2024-02-08 13:06:26 +0300

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Navbar =()=>{
+
+  return(
+  <nav className="navbar">
+    <h2>Blog Post</h2>
+    <div className="links">
+      <a href="#">Home</a>
+      <a href="#">New Blog</a>
+    </div>
+  </nav>
+  )
+}
+export default Navbar;
