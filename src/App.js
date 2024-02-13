026f3705c9d74c56233224c5fbd5f65bf754707a
JJ <jumakrk@gmail.com>
2024-02-13 11:32:00 +0300

    added a data folder with the file db.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Content from './components/Content';
+import Blog from './components/Blogs';
 import Navbar from  "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <div className='content'></div>
+      <Content/>
+      <Blog/>
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const UserInput  = () => {
+  const [Title, setTitle] = useState("");
+  const [Author, setAuthor] = useState("");
+  const [Body, setBody] = useState("");
+
+    return(
+      <div>
+        <form>
+        <label>Title</label><br></br>
+        <input className="content" name="Title" type="text" placeholder="Title" required value={Title} onChange={(e)=>setTitle(e.target.value)}></input> <br></br>
+
+        <label>Author</label><br></br>
+        <input className="content" name="Author" type="text" placeholder="Author" required value={Author} onChange={(e)=>setAuthor(e.target.value)}></input> <br></br>
+
+        <label>Body</label><br></br>
+        <input className="content" name="Body" type="text" placeholder="Body" required value={Body} onChange={(e)=>setBody(e.target.value)}></input> <br></br>
+
+        <button>Save Blog</button>
+        </form>
+      </div>
+    )
+};
+export default UserInput;
